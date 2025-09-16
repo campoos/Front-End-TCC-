148ee116498333b711af28fc04c690057550f5a6
campoos <joaosantos20071009@gmail.com>
2025-09-16 17:08:52 -0300

    FEAT primeiro container home

---
+++ analityca-ai/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div id="container1">
+        <div id="header">
+          <div id="logo">
+            <h1>Analytica AI</h1>
+          </div>
+          <div id="links">
+            <a href="" className="link">Recursos</a>
+            <a href="" className="link">Benefícios</a>
+            <a href="" className="link">Contato</a>
+          </div>
+          <div id="botaoContainer">
+            <button id="login">Login</button>
+          </div>
+        </div>
+        <div id="containerTextoHome">
+          <h1>A inteligência que transforma dados em resultados</h1>
+          <h2>Transformando dados em aprendizado inteligente.</h2>
+          <div id="botaoContainer">
+            <button id="comecarAgora">Começar Agora</button>
+          </div>
+        </div>
+      </div>
+      <div id="container2">
+
+      </div>
+    </>
+  )
+}
+
+export default App
