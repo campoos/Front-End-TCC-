10ca60cc4bbbf99f1307b1e463473f6dfd1560ff
JOÃO VICTOR CAMPOS DOS SANTOS <24122460@dsn127m1246726.cfp127aluno.local>
2025-10-16 17:15:51 -0300

    WIP - tela de configuracoes

--- analityca-ai/src/pages/configuracoes/Configuracoes.jsx
+++ analityca-ai/src/pages/configuracoes/Configuracoes.jsx
@@ -1,5 +1,8 @@
 import './Configuracoes.css'
 import Sidebar from '../../components/sidebar/Sidebar'
+import iconUser from '../../assets/configuracoes-icons/user-icon.png'
+import iconEmail from '../../assets/configuracoes-icons/email-icon.png'
+import iconPhone from '../../assets/configuracoes-icons/phone-icon.png'
 
 function ConfiguracoesPage() {
   const dataUser = JSON.parse(localStorage.getItem("userData"))
@@ -11,31 +14,45 @@ function ConfiguracoesPage() {
                 <h1 id='title'>Configurações Do Aluno: {dataUser.nome}</h1>
                 <hr/>
               <div id='configuracoes'>
-                <h3 id='informacaoAluno'>Informações do Aluno</h3>
                 <div id='containerInformacoes'>
+                  <h3 id='informacaoAluno'>Informações do Aluno</h3>
                   <form action="">
                     <div className='grupoInput'> 
                       <label HTMLfor="nome">Nome</label>
-                      <input type="text" id='nome' name='nome' placeholder='Nome do aluno' required/>
+                      <div className="inputContainer">
+                        <img src={iconUser} alt="" />
+                        <input type="text" id='nome' name='nome' placeholder='Nome do aluno' required/>
+                      </div>
                     </div>
                     <div className='grupoInput'> 
                       <label HTMLfor="email">Email de Contato</label>
-                      <input type="email" id='email' name='email' placeholder='contato@modelo.com' required/>
+                      <div className="inputContainer">
+                        <img src={iconEmail} alt="" />
+                        <input type="email" id='email' name='email' placeholder='contato@modelo.com' required/>
+                      </div>
                     </div>
                     <div className='grupoInput'> 
                       <label HTMLfor="telefone">Telefone de Contato</label>
-                      <input type="tel" id='telefone' name='telefone' placeholder='(11) 9 00000-0000' required/>
-                    </div>
-                    <hr/>
-                    <div id='containerModo'>
-                      <label HTMLfor="modoDark">Modo Dark</label>
-                    </div>
-                    <span>Troca as cores da tela para um modo escuro</span>
-                    <div id='containerButton'>
-                      <button type='submit'>Salvar Alterações</button>
+                      <div className="inputContainer">
+                        <img src={iconPhone} alt="" />
+                        <input type="tel" id='telefone' name='telefone' placeholder='(11) 9 00000-0000' required/>
+                      </div>
                     </div>
                   </form>
                 </div>
+                <hr />
+                <div id="containerAparencia">
+                  <div id='containerTema'> 
+                    <div id="containerTextoTema">
+                      <label HTMLfor="modoDark">Modo Dark</label>
+                      <span>Troca as cores da tela para um modo escuro</span>
+                    </div>
+                    <input type="checkbox" />
+                  </div>
+                </div>
+                <div id='containerButton'>
+<button type='submit'>Salvar Alterações</button>
+</div>
               </div>
             </div>
         </div>
