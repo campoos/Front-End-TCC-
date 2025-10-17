06b99e0521b2969bd8c62ce29d3c350abf3fb9f2
Campoos <joaosantos20071009@gmail.com>
2025-10-17 08:03:14 -0300

    FEAT - Tela de configurações

--- analityca-ai/src/pages/configuracoes/Configuracoes.jsx
+++ analityca-ai/src/pages/configuracoes/Configuracoes.jsx
@@ -14,45 +14,45 @@ function ConfiguracoesPage() {
                 <h1 id='title'>Configurações Do Aluno: {dataUser.nome}</h1>
                 <hr/>
               <div id='configuracoes'>
-                <div id='containerInformacoes'>
-                  <h3 id='informacaoAluno'>Informações do Aluno</h3>
-                  <form action="">
+                <form action="">
+                  <div id='containerInformacoes'>
+                    <h3 id='informacaoAluno'>Informações do Aluno</h3>
                     <div className='grupoInput'> 
-                      <label HTMLfor="nome">Nome</label>
+                      <label htmlFor="nome">Nome</label>
                       <div className="inputContainer">
                         <img src={iconUser} alt="" />
                         <input type="text" id='nome' name='nome' placeholder='Nome do aluno' required/>
                       </div>
                     </div>
                     <div className='grupoInput'> 
-                      <label HTMLfor="email">Email de Contato</label>
+                      <label htmlFor="email">Email de Contato</label>
                       <div className="inputContainer">
                         <img src={iconEmail} alt="" />
                         <input type="email" id='email' name='email' placeholder='contato@modelo.com' required/>
                       </div>
                     </div>
                     <div className='grupoInput'> 
-                      <label HTMLfor="telefone">Telefone de Contato</label>
+                      <label htmlFor="telefone">Telefone de Contato</label>
                       <div className="inputContainer">
                         <img src={iconPhone} alt="" />
                         <input type="tel" id='telefone' name='telefone' placeholder='(11) 9 00000-0000' required/>
                       </div>
                     </div>
-                  </form>
-                </div>
-                <hr />
-                <div id="containerAparencia">
-                  <div id='containerTema'> 
-                    <div id="containerTextoTema">
-                      <label HTMLfor="modoDark">Modo Dark</label>
-                      <span>Troca as cores da tela para um modo escuro</span>
-                    </div>
-                    <input type="checkbox" />
                   </div>
-                </div>
-                <div id='containerButton'>
-<button type='submit'>Salvar Alterações</button>
-</div>
+                  <hr />
+                  <div id="containerAparencia">
+                    <div id='containerTema'> 
+                      <div id="containerTextoTema">
+                        <label htmlFor="modoDark">Modo Dark</label>
+                        <span>Troca as cores da tela para um modo escuro</span>
+                      </div>
+                      <input type="checkbox" />
+                    </div>
+                  </div>
+                  <div id='containerButton'>
+                    <button type='submit'>Salvar Alterações</button>
+                  </div>
+                </form>
               </div>
             </div>
         </div>
