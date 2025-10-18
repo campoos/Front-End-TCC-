01a282900d510c70e97f7352308f989e51916f90
Campoos <joaosantos20071009@gmail.com>
2025-10-18 17:28:19 -0300

    FEAT - tela estatica de configurações

--- analityca-ai/src/pages/configuracoes/Configuracoes.jsx
+++ analityca-ai/src/pages/configuracoes/Configuracoes.jsx
@@ -11,48 +11,56 @@ function ConfiguracoesPage() {
         <div id="telaConfiguracoes">
             <Sidebar/>
             <div id="containerConfiguracoes">
+              <div id="containerTexto">
                 <h1 id='title'>Configurações Do Aluno: {dataUser.nome}</h1>
-                <hr/>
+                <span id='desc'>Gerencie suas informações e preferências do sistema</span>
+              </div>
               <div id='configuracoes'>
-                <form action="">
-                  <div id='containerInformacoes'>
-                    <h3 id='informacaoAluno'>Informações do Aluno</h3>
-                    <div className='grupoInput'> 
-                      <label htmlFor="nome">Nome</label>
-                      <div className="inputContainer">
-                        <img src={iconUser} alt="" />
-                        <input type="text" id='nome' name='nome' placeholder='Nome do aluno' required/>
-                      </div>
-                    </div>
-                    <div className='grupoInput'> 
-                      <label htmlFor="email">Email de Contato</label>
-                      <div className="inputContainer">
-                        <img src={iconEmail} alt="" />
-                        <input type="email" id='email' name='email' placeholder='contato@modelo.com' required/>
-                      </div>
-                    </div>
-                    <div className='grupoInput'> 
-                      <label htmlFor="telefone">Telefone de Contato</label>
-                      <div className="inputContainer">
-                        <img src={iconPhone} alt="" />
-                        <input type="tel" id='telefone' name='telefone' placeholder='(11) 9 00000-0000' required/>
-                      </div>
+                <div id='containerInformacoes'>
+                  <h3 id='informacaoAluno'>Informações do Aluno</h3>
+                  <div className='grupoInput'> 
+                    <label htmlFor="nome">Nome</label>
+                    <div className="inputContainer">
+                      <img src={iconUser} alt="" />
+                      <input type="text" id='nome' name='nome' placeholder='Nome do aluno' required/>
                     </div>
                   </div>
-                  <hr />
-                  <div id="containerAparencia">
-                    <div id='containerTema'> 
-                      <div id="containerTextoTema">
-                        <label htmlFor="modoDark">Modo Dark</label>
-                        <span>Troca as cores da tela para um modo escuro</span>
-                      </div>
-                      <input type="checkbox" />
+                  <div className='grupoInput'> 
+                    <label htmlFor="email">Email de Contato</label>
+                    <div className="inputContainer">
+                      <img src={iconEmail} alt="" />
+                      <input type="email" id='email' name='email' placeholder='contato@modelo.com' required/>
                     </div>
                   </div>
-                  <div id='containerButton'>
-                    <button type='submit'>Salvar Alterações</button>
+                  <div className='grupoInput'> 
+                    <label htmlFor="telefone">Telefone de Contato</label>
+                    <div className="inputContainer">
+                      <img src={iconPhone} alt="" />
+                      <input type="tel" id='telefone' name='telefone' placeholder='(00) 0 00000-0000' required/>
+                    </div>
                   </div>
-                </form>
+                </div>
+                <div id="containerAparencia">
+                  <div id='containerTema'> 
+                    <div id="containerTextoTema">
+                      <h3>Modo Dark</h3>
+                      <span>Troca as cores da tela para um modo escuro</span>
+                    </div>
+                    <div className="switch-container">
+                      <input type="checkbox" id="meuSwitch" className="switch-checkbox"></input>
+                      
+                      <label htmlFor="meuSwitch" className="switch-label">
+                          <span className="switch-slider"></span>
+                      </label>
+                  </div>
+                  </div>
+                </div>
+              </div>
+              <div id='containerButton'>
+                <button type='submit'>Salvar Alterações</button>
+              </div>
+              <div id='containerLogout'>
+                <button type='submit'>Sair da conta</button>
               </div>
             </div>
         </div>
