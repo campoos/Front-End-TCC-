2bfa879cc922b3a9fa68ef07f9d2877b1e68079e
Campoos <joaosantos20071009@gmail.com>
2025-10-19 19:11:18 -0300

    FEAT - modal de confirmação de logout

--- analityca-ai/src/pages/configuracoes/Configuracoes.jsx
+++ analityca-ai/src/pages/configuracoes/Configuracoes.jsx
@@ -5,79 +5,126 @@ import iconEmail from '../../assets/configuracoes-icons/email-icon.png'
 import iconPhone from '../../assets/configuracoes-icons/phone-icon.png'
 
 import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+
+const FundoEscuro = ({ show }) => {
+  if (!show) return null; 
+
+  return (<div id="fundoEscuro"></div>);
+};
+
+const ModalConfirmacaoLogout = ({ show, onCancel, onConfirm }) => {
+  if (!show) return null;
+
+  return (
+    <div id="modalContainer">
+      <div id="modalContent">
+        <h3>Confirmar Saída</h3>
+        <p>Você tem certeza que deseja <strong>sair da sua conta?</strong>. Dados não salvos e preferências temporárias armazenadas neste navegador serão perdidas.</p>
+        <div id="modalOptions">
+          <button onClick={onCancel} id="buttonCancelar">Cancelar</button> 
+          <button onClick={onConfirm} id="buttonConfirmar">Confirmar</button> 
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function ConfiguracoesPage() {
 
-  const handleLogout = () => {
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
+
+  const navigate = useNavigate();
+  const dataUser = JSON.parse(localStorage.getItem("userData"));
+
+  const executeLogout = () => {
     localStorage.removeItem("userData");
     sessionStorage.removeItem("userData");
-    
-    localStorage.removeItem("lembrarCredencial"); 
-
+    localStorage.removeItem("lembrarCredencial");
     navigate("/login");
   };
 
-  const dataUser = JSON.parse(localStorage.getItem("userData"))
-  const navigate = useNavigate()
+  const handleOpenLogoutModal = () => {
+    setShowLogoutModal(true);
+  };
+
+  const handleCloseLogoutModal = () => {
+    setShowLogoutModal(false);
+  };
+
+  const handleConfirmLogout = () => {
+    executeLogout();
+    handleCloseLogoutModal();
+  };
 
   return (
     <>
         <div id="telaConfiguracoes">
-            <Sidebar/>
-            <div id="containerConfiguracoes">
-              <div id="containerTexto">
-                <h1 id='title'>Configurações Do Aluno: {dataUser.nome}</h1>
-                <span id='desc'>Gerencie suas informações e preferências do sistema</span>
-              </div>
-              <div id='configuracoes'>
-                <div id='containerInformacoes'>
-                  <h3 id='informacaoAluno'>Informações do Aluno</h3>
-                  <div className='grupoInput'> 
-                    <label htmlFor="nome">Nome</label>
-                    <div className="inputContainer">
-                      <img src={iconUser} alt="" />
-                      <input type="text" id='nome' name='nome' placeholder='Nome do aluno' value={dataUser.nome} required/>
-                    </div>
-                  </div>
-                  <div className='grupoInput'> 
-                    <label htmlFor="email">Email de Contato</label>
-                    <div className="inputContainer">
-                      <img src={iconEmail} alt="" />
-                      <input type="email" id='email' name='email' placeholder='contato@modelo.com' value={dataUser.email} required/>
-                    </div>
-                  </div>
-                  <div className='grupoInput'> 
-                    <label htmlFor="telefone">Telefone de Contato</label>
-                    <div className="inputContainer">
-                      <img src={iconPhone} alt="" />
-                      <input type="tel" id='telefone' name='telefone' placeholder='(00) 0 00000-0000' value={dataUser.telefone} required/>
-                    </div>
+          <Sidebar/>
+          <div id="containerConfiguracoes">
+            <div id="containerTexto">
+              <h1 id='title'>Configurações Do Aluno: {dataUser.nome}</h1>
+              <span id='desc'>Gerencie suas informações e preferências do sistema</span>
+            </div>
+            <div id='configuracoes'>
+              <div id='containerInformacoes'>
+                <h3 id='informacaoAluno'>Informações do Aluno</h3>
+                <div className='grupoInput'> 
+                  <label htmlFor="nome">Nome</label>
+                  <div className="inputContainer">
+                    <img src={iconUser} alt="" />
+                    <input type="text" id='nome' name='nome' placeholder='Nome do aluno' value={dataUser.nome} required/>
                   </div>
                 </div>
-                <div id="containerAparencia">
-                  <div id='containerTema'> 
-                    <div id="containerTextoTema">
-                      <h3>Modo Dark</h3>
-                      <span>Troca as cores da tela para um modo escuro</span>
-                    </div>
-                    <div className="switch-container">
-                      <input type="checkbox" id="meuSwitch" className="switch-checkbox"></input>
-                      
-                      <label htmlFor="meuSwitch" className="switch-label">
-                          <span className="switch-slider"></span>
-                      </label>
+                <div className='grupoInput'> 
+                  <label htmlFor="email">Email de Contato</label>
+                  <div className="inputContainer">
+                    <img src={iconEmail} alt="" />
+                    <input type="email" id='email' name='email' placeholder='contato@modelo.com' value={dataUser.email} required/>
                   </div>
+                </div>
+                <div className='grupoInput'> 
+                  <label htmlFor="telefone">Telefone de Contato</label>
+                  <div className="inputContainer">
+                    <img src={iconPhone} alt="" />
+                    <input type="tel" id='telefone' name='telefone' placeholder='(00) 0 00000-0000' value={dataUser.telefone} required/>
                   </div>
                 </div>
               </div>
-              <div id='containerButton'>
-                <button type='submit'>Salvar Alterações</button>
-              </div>
-              <div id='containerLogout'>
-                <button type='button' onClick={handleLogout}>Sair da conta</button>
+              <div id="containerAparencia">
+                <div id='containerTema'> 
+                  <div id="containerTextoTema">
+                    <h3>Modo Dark</h3>
+                    <span>Troca as cores da tela para um modo escuro</span>
+                  </div>
+                  <div className="switch-container">
+                    <input type="checkbox" id="meuSwitch" className="switch-checkbox"></input>
+                    
+                    <label htmlFor="meuSwitch" className="switch-label">
+                        <span className="switch-slider"></span>
+                    </label>
+                </div>
+                </div>
               </div>
             </div>
-        </div>
+            <div id='containerButton'>
+              <button type='submit'>Salvar Alterações</button>
+            </div>
+            <div id='containerLogout'>
+              <button type='button' onClick={handleOpenLogoutModal}>Sair da conta</button>
+            </div>
+
+            <FundoEscuro 
+              show={showLogoutModal} 
+            />
+
+            <ModalConfirmacaoLogout
+              show={showLogoutModal}
+              onCancel={handleCloseLogoutModal} 
+              onConfirm={handleConfirmLogout} 
+            />
+          </div>
+      </div>
     </>
   )
 }
